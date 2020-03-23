089b4f7bb4e47506e006bc9605e6bd56445ffe6b
CMT-test1 <guillaumeorsal+cmttest1@gmail.com>
2020-03-23 17:47:04 +0100

    Constant to string to avoid warning

--- start.php
+++ start.php
@@ -24,40 +24,40 @@ function elgg_cookieconsent_init() {
 
 function elgg_cookieconsent_config_site($hook, $type, $value, $params) {
     
-    $message = elgg_get_plugin_setting('message', elgg_cookieconsent);
+    $message = elgg_get_plugin_setting('message', 'elgg_cookieconsent');
     if (!($message)){
 	$message = elgg_echo('elgg_cookieconsent:message');
     }
-    $dismiss = elgg_get_plugin_setting('dismiss', elgg_cookieconsent);
+    $dismiss = elgg_get_plugin_setting('dismiss', 'elgg_cookieconsent');
     if (!($dismiss)){
 	$dismiss = elgg_echo('elgg_cookieconsent:dismiss');
     }
-    $url = elgg_get_plugin_setting('url', elgg_cookieconsent);
-    $learnMore = elgg_get_plugin_setting('learnMore', elgg_cookieconsent);
+    $url = elgg_get_plugin_setting('url', 'elgg_cookieconsent');
+    $learnMore = elgg_get_plugin_setting('learnMore', 'elgg_cookieconsent');
     if (!($learnMore)){
 	$learnMore = elgg_echo('elgg_cookieconsent:learnmore');
     }
-    $theme = elgg_get_plugin_setting('theme', elgg_cookieconsent);
+    $theme = elgg_get_plugin_setting('theme', 'elgg_cookieconsent');
     if (!($theme)){
 	$theme = 'block';
     }
-    $position = elgg_get_plugin_setting('position', elgg_cookieconsent);
+    $position = elgg_get_plugin_setting('position', 'elgg_cookieconsent');
     if (!($position)){
 	$position = 'bottom';
     }
-    $popup_background_color = elgg_get_plugin_setting('popup_background_color', elgg_cookieconsent);
+    $popup_background_color = elgg_get_plugin_setting('popup_background_color', 'elgg_cookieconsent');
     if (!($popup_background_color) || !preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $popup_background_color)){
 	$popup_background_color = '#000000';
     }
-    $popup_text_color = elgg_get_plugin_setting('popup_text_color', elgg_cookieconsent);
+    $popup_text_color = elgg_get_plugin_setting('popup_text_color', 'elgg_cookieconsent');
     if (!($popup_text_color) || !preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $popup_text_color)){
 	$popup_text_color = '#FFFFFF';
     }
-    $button_background_color = elgg_get_plugin_setting('button_background_color', elgg_cookieconsent);
+    $button_background_color = elgg_get_plugin_setting('button_background_color', 'elgg_cookieconsent');
     if (!($button_background_color) || !preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $button_background_color)){
 	$button_background_color = '#F1D600';
     }
-    $button_text_color = elgg_get_plugin_setting('button_text_color', elgg_cookieconsent);
+    $button_text_color = elgg_get_plugin_setting('button_text_color', 'elgg_cookieconsent');
     if (!($button_text_color) || !preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $button_text_color)){
 	$button_text_color = '#000000';
     }
